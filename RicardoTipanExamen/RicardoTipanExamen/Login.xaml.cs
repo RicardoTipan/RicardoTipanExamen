45a8c958897eaba64a452b835bdc181a86337510
Ricardo Tipàn Toapanta <javi_asm@hotmail.com>
2020-10-31 01:09:20 -0500

    Agregar archivos de proyecto.

---
+++ RicardoTipanExamen/RicardoTipanExamen/Login.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RicardoTipanExamen
+{
+    public partial class MainPage : ContentPage
+    {
+        string errorMessage = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void limpiar()
+        {
+            txtUsu.Text = string.Empty;
+            txtCon.Text = string.Empty;
+        }
+
+        private async void btnIng_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsu.Text;
+            string contraseña = txtCon.Text;
+
+            if (txtUsu.Text == "est" && txtCon.Text == "uis")
+            {
+                await DisplayAlert("Acceso Correcto", errorMessage, "OK");
+                await Navigation.PushAsync(new Registro(usuario, contraseña));
+            }
+
+            else
+            {
+                await DisplayAlert("Usuario o Contraseña Incorrecta", errorMessage, "OK");
+                await Navigation.PushAsync(new MainPage());
+                limpiar();
+
+            }
+        }
+    }
+}
---
+++ RicardoTipanExamen/RicardoTipanExamen/Registro.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RicardoTipanExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro( string usuario, string contraseña)
+        {
+            InitializeComponent();
+            
+            lblUsuario.Text = usuario;
+
+        }
+
+        private void bntCal_Clicked(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                double nCuota = Convert.ToDouble(txtCuo.Text);
+                double resultado = 0;
+                if (nCuota <= 1800)
+                {
+                    resultado = nCuota / 1800;
+                    txtCuo.Text = resultado.ToString();
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de Alerta", ex.Message, "Aceptar");
+            }
+
+
+        }
+    }
+}
